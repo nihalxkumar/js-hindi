3e6b6d6c41353e7397a5f7c30c7a79f1de4bc7db
Nihal  Kumar <121309701+nihalxkumar@users.noreply.github.com>
2022-12-31 11:55:42 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,36 @@
+let score = "hitesh"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber); for finding type
+// console.log(valueInNumber); for finding output
+// "hitesh" = NaN
+// "33" => 33
+// "33abc" => NaN
+// null => 0
+// undefined => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "hitesh"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn);
+// "hitesh" => true
+// "" => false
+// 1 => true   0 => false
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);  33
+// console.log(typeof stringNumber); type of ayega number. kyuki humne String() mein convert kiya
+
+
+
+
+
+
+
+
+

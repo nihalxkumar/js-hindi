39b65f8887bdc136230e3597f5929ea26949db77
Nihal  Kumar <121309701+nihalxkumar@users.noreply.github.com>
2023-01-18 14:32:20 +0000

    Comaprision, equality and strict check

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,29 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0); // null ko 0 mein covert kardiya gaya 0>=0
+
+// The reason is that an equality check == and comparisons ><>=<= work differently. 
+// comparisons convert null to a number, treating it as 0
+// That's why (3) null >=0 is true and (1) null > O is false.
+
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === 
+
+console.log("2" === 2);
+
+
+// video ke end mein conclusion
